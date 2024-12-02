7e1813f80554b76171e8751d597679f0cec3d8e5
MylesBP <bohrerpu@grinnell.edu>
2024-12-02 17:19:29 -0600

    Implemented BitTree and the corresponding node helper

--- src/main/java/edu/grinnell/csc207/util/BitTree.java
+++ src/main/java/edu/grinnell/csc207/util/BitTree.java
@@ -1,63 +1,161 @@
 package edu.grinnell.csc207.util;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 /**
  * Trees intended to be used in storing mappings between fixed-length 
  * sequences of bits and corresponding values.
  *
- * @author Your Name Here
+ * @author Myles Bohrer-Purnell
  */
 public class BitTree {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
 
+  /** Starting node for the tree. */
+  BitTreeNode startNode;
+
+  /** Expected length of the input. */
+  int num;
+
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
 
   /**
-   *
+   * Creates a new bit tree with expected input size n.
+   * @param n the expected input size
    */
   public BitTree(int n) {
+    this.num = n;
+    this.startNode = new BitTreeNode(null, null, null);
   } // BitTree(int)
 
   // +---------------+-----------------------------------------------
   // | Local helpers |
   // +---------------+
 
+  public void dumpHelper(PrintWriter pen, BitTreeNode curNode, String curResultOrig) {
+    String curResult = curResultOrig;
+    if (curNode != null) {
+      if (curNode.getValue() != null) {
+        pen.println(curResult + "," + curNode.getValue());
+      } else {
+        curResult = curResultOrig.concat("0");
+        dumpHelper(pen, curNode.getLeft(), curResult);
+        curResult = curResultOrig.concat("1");
+        dumpHelper(pen, curNode.getRight(), curResult);
+      } // if/else
+    } // if
+  } // dumpHelper
+
   // +---------+-----------------------------------------------------
   // | Methods |
   // +---------+
 
+
   /**
-   *
+   * Follows the path through the tree given by bits,
+   * adding nodes as appropriate and adding or replaceing the
+   * value at the end with value.
+   * @param bits the path that the value will be added at the end of
+   * @param value the value to be added at the end
    */
-  public void set(String bits, String value) {
-    // STUB
+  public void set(String bits, String value) throws IndexOutOfBoundsException {
+    if (bits.length() != this.num) {
+      throw new IndexOutOfBoundsException("Wrong Lengthed Path Input");
+    } // if
+    BitTreeNode curNode = startNode;
+    BitTreeNode prevNode = startNode;
+    for (int i = 0; i < this.num; i++) {
+      if (bits.charAt(i) == '0') {
+        // Move left
+        prevNode = curNode;
+        curNode = curNode.getLeft();
+        // handle null conditions for the new node
+        if (curNode == null) {
+          curNode = new BitTreeNode(null, null, null);
+        } // if
+        prevNode.setLeft(curNode);
+      } else if (bits.charAt(i) == '1') {
+        // Move right
+        prevNode = curNode;
+        curNode = curNode.getRight();
+        // handle null conditions for the new node
+        if (curNode == null) {
+          curNode = new BitTreeNode(null, null, null);
+        } // if
+        prevNode.setRight(curNode);
+      } else {
+        throw new IndexOutOfBoundsException("Input Path Does Not Only Include 0s and 1s");
+      } // if/else
+    } // for
+    curNode.setValue(value);
   } // set(String, String)
 
   /**
-   *
+   * Follows the path through the tree given by bits, returning
+   * the value at the end.
+   * @param bits the path to the desired value
+   * @return the value at the end of the path or an exception if incorrect path
    */
   public String get(String bits) {
-    return "";  // STUB
+    if (bits.length() != this.num) {
+      throw new IndexOutOfBoundsException("Wrong Lengthed Path Input");
+    } // if
+    BitTreeNode curNode = startNode;
+    for (int i = 0; i < this.num; i++) {
+      if (bits.charAt(i) == '0') {
+        // Move left
+        curNode = curNode.getLeft();
+      } else if (bits.charAt(i) == '1') {
+        // Move right
+        curNode = curNode.getRight();
+      } else {
+        // input path value is not 0 or 1
+        throw new IndexOutOfBoundsException("Input Path Does Not Only Include 0s and 1s");
+      } // if/else
+
+      if (curNode == null) {
+        // if the path does not exist because node is null
+        throw new IndexOutOfBoundsException("Input Path Does Not Exist");
+      } // if
+    } // for
+    return curNode.getValue();
   } // get(String, String)
 
   /**
-   *
+   * Prints out the contents of the tree in CSV format.
+   * @param pen pen for printing output
    */
   public void dump(PrintWriter pen) {
-    // STUB
+    BitTreeNode curNode = startNode;
+    dumpHelper(pen, curNode.getRight(), "1");
+    dumpHelper(pen, curNode.getLeft(), "0");
   } // dump(PrintWriter)
 
   /**
-   *
+   * Reads a series of lines of
+   * the form bits,value and stores them in the tree.
+   * @param source stream of input to read
    */
   public void load(InputStream source) {
-    // STUB
+    InputStreamReader isr = new InputStreamReader(source);
+    BufferedReader br = new BufferedReader(isr);
+    String line;
+    String[] bitVal;
+    try {
+      while ((line = br.readLine()) != null) {
+        bitVal = line.split(",");
+        set(bitVal[0], bitVal[1]);
+      } // while
+    } catch(Exception eof) {
+      // end the proccess if issue
+    } // try/catch
   } // load(InputStream)
 
 } // class BitTree
---
+++ src/main/java/edu/grinnell/csc207/util/BitTreeNode.java
@@ -0,0 +1,72 @@
+package edu.grinnell.csc207.util;
+
+public class BitTreeNode {
+
+  /** Child node to left of the current node. */
+  BitTreeNode leftNode;
+
+  /** Child node to the right of the current node. */
+  BitTreeNode rightNode;
+
+  /** The letter that the previous path led to. */
+  String value;
+
+  /**
+   * Constructs a new current node, with pointers to the nodes on either side of it.
+   * @param lNode the left child
+   * @param rNode the right child
+   */
+  public BitTreeNode(BitTreeNode lNode, BitTreeNode rNode, String val) {
+    this.leftNode = lNode;
+    this.rightNode = rNode;
+    this.value = val;
+  } // BitTreeInterorNode
+
+  /**
+   * Changes the node on the right of the current node.
+   * @param newRight new right child node
+   */
+  public void setRight(BitTreeNode newRight) {
+    this.rightNode = newRight;
+  } // setRight
+
+  /**
+   * Changes the node on the left side of the current node.
+   * @param newLeft new left child node
+   */
+  public void setLeft(BitTreeNode newLeft) {
+    this.leftNode = newLeft;
+  } // setLeft
+
+  /**
+   * Returns the node to the right of the current node.
+   * @return the node on the right
+   */
+  public BitTreeNode getRight() {
+    return this.rightNode;
+  } // getRight
+
+  /**
+   * Returns the node to the left of the current node.
+   * @return the node on the left
+   */
+  public BitTreeNode getLeft() {
+    return this.leftNode;
+  } // getLeft
+
+    /**
+   * Returns the leaf's String value.
+   * @return the value at the leaf
+   */
+  public String getValue() {
+    return this.value;
+  } // getValue
+
+  /**
+   * Sets the value at the leaf to a new value.
+   * @param newVal the new value for the leaf
+   */
+  public void setValue(String newVal) {
+    this.value = newVal;
+  } // setValue
+} // BitTreeNode
